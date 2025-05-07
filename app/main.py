38a19fa689b67a12851238acf008b1caddc01512
yerayd <yeray.navarro@outlook.com>
2025-05-07 18:05:23 +0200

    added cors policy and more data

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api import routes_product, routes_season, routes_region
 
 app = FastAPI(
@@ -6,7 +7,13 @@ app = FastAPI(
     description="Devuelve productos de temporada según región y temporada",
     version="1.0.0"
 )
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(routes_product.router, prefix="/api")
 app.include_router(routes_season.router, prefix="/api")
 app.include_router(routes_region.router, prefix="/api")
